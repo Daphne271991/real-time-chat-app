195570c644e948e0fc4fda31ada9d4ee3b14c9d7
Daphne271991 <134757720+Daphne271991@users.noreply.github.com>
2023-08-20 12:16:22 -0600

    added firebase auth

---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7D6luI6kH-UFwwHIWeh_kkX1ptJHrcmI",
+  authDomain: "chatapp-993d4.firebaseapp.com",
+  projectId: "chatapp-993d4",
+  storageBucket: "chatapp-993d4.appspot.com",
+  messagingSenderId: "593882052231",
+  appId: "1:593882052231:web:ea195c133b636113736a22",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
